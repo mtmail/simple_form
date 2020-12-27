b7aca5a6c7e9c876733efbebfb67e894427fcebc
marc tobias <mtmail@gmx.net>
2020-12-28 00:15:37 +0100

    disable cache

--- lib/simple_form/i18n_cache.rb
+++ lib/simple_form/i18n_cache.rb
@@ -4,20 +4,24 @@ module SimpleForm
   # like boolean collection, required string. This module provides
   # caching facility to speed up form construction.
   module I18nCache
+    @@our_cache = {}
+
     def i18n_cache(key)
       get_i18n_cache(key)[I18n.locale] ||= yield.freeze
     end
 
     def get_i18n_cache(key)
-      if class_variable_defined?(:"@@#{key}")
-        class_variable_get(:"@@#{key}")
+      return {}
+
+      if @@our_cache.key?(key)
+        @@our_cache.fetch(key)
       else
         reset_i18n_cache(key)
       end
     end
 
     def reset_i18n_cache(key)
-      class_variable_set(:"@@#{key}", {})
+      @@our_cache[key] = {}
     end
   end
 end
